da912d4d0a4c6641ebd366e72180a752fc6ab01c
MrCopperrrr <ntphat0802@gmail.com>
2025-07-02 14:16:27 +0700

    Upload Power_function.cpp

---
+++ Lab 1/Recursion/Power_function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int power(int a, int b){
+    if (b == 0)  return 1;
+    return a * power(a, b-1);
+}
+
+int main(){
+    int a, b;
+    cout << power(2, 5) << endl;
+    cout << power(3, 3) << endl;
+    cout << power(5, 0) << endl; 
+    cout << power(10, 10) << endl;
+    cout << power(2, 10) << endl;
+
+    return 0;
+}
