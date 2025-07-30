8707d4dc88cd9563bf489e1313c061155926ce1f
MrCopperrrr <ntphat0802@gmail.com>
2025-07-30 23:35:09 +0700

    add mix test for avl tree

--- Lab 4/AVL/main.cpp
+++ Lab 4/AVL/main.cpp
@@ -16,6 +16,7 @@ int main() {
     }
     //2.2
     cout << "\n=== Duplicate Insert Test ===\n";
+    cout << "Insert 13: ";
     tree.insert(13); 
     tree.printLNR();  
 
@@ -24,6 +25,56 @@ int main() {
 
     int to_remove[] = {13, 4, 21};  
     for (int x : to_remove) {
+        cout << "Remove " << x << ": ";
+        tree.remove(x);
+        tree.printLNR();
+    }
+
+    //mix test
+    cout << "\n=== Mixed Insert and Remove Test ===\n";
+    int rand_ins_val[] = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60};
+    for (int i = 0; i < 12; ++i) {
+        cout << "Insert " << rand_ins_val[i] << ": ";
+        tree.insert(rand_ins_val[i]);
+        tree.printLNR();
+    }
+    int rand_rem_val[] = {10, 20, 30, 40, 50, 72, 33, 19, 60, 59, 21, 17};
+    for (int i = 0; i < 12; ++i) {
+        cout << "Remove " << rand_rem_val[i] << ": ";
+        tree.remove(rand_rem_val[i]);
+        tree.printLNR();
+    }
+    cout << "Insert multiple 13: ";
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+    tree.insert(13);
+
+    tree.printLNR();
+
+    for (int i = 0; i < 20; ++i) {
+        cout << "Remove 13: ";
+        tree.remove(13);
+        tree.printLNR();
+    }
+
+    int remove_all[] = {1, 5, 13, 15, 25, 35, 45, 55, 60, 71, 87, 91};  
+    for (int x : remove_all) {
         cout << "Remove " << x << ": ";
         tree.remove(x);
         tree.printLNR();
