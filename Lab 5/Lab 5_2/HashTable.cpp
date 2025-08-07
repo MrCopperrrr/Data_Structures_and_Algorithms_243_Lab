13ab401d116b2d6fbafeca80e82e5a9b04641ece
MrCopperrrr <ntphat0802@gmail.com>
2025-08-07 11:30:54 +0700

    Upload Lab 5_2

--- Lab 5/Lab 5_2/HashTable.cpp
+++ Lab 5/Lab 5_2/HashTable.cpp
@@ -2,7 +2,7 @@
 #define __HASHTABLE__CPP__
 
 #include "HashTable.h"
-#include <iostream.h>
+#include <iostream>
 
 template <class T>
 HashTable<T>::HashTable()
@@ -52,15 +52,34 @@ int HashTable<T>::moduloDivision(int key)
 template <class T>
 int HashTable<T>::midSquare(int key)
 {
-	// write your code here
-	return -1;
+	// Mid-square method: square the key, extract middle digits
+	if (key < 0) key = -key;
+	long long squared = (long long)key * (long long)key;
+	// Extract middle digits: for simplicity, get 2 or 3 digits from the middle
+	int numDigits = 0;
+	long long temp = squared;
+	while (temp > 0) {
+		temp /= 10;
+		numDigits++;
+	}
+	int mid = numDigits / 2;
+	int extractDigits = 2; // extract 2 digits from the middle
+	long long divisor = 1;
+	for (int i = 0; i < mid - extractDigits/2; i++) divisor *= 10;
+	int midVal = (int)((squared / divisor) % (int)pow(10, extractDigits));
+	return midVal % numBucks;
 }
 //---------------------------------------------------------
 template <class T>
 int HashTable<T>::pseudoRandom(int key)
 {	
-	// write your code here
-	return -1; 
+	// Simple linear congruential generator: (a*key + b) % numBucks
+	// Choose two prime numbers for a and b
+	const int a = 31; // prime
+	const int b = 17; // prime
+	if (key < 0) key = -key;
+	int val = (a * key + b) % numBucks;
+	return val;
 }
 //---------------------------------------------------------
 template <class T>
@@ -68,13 +87,26 @@ bool HashTable<T>::insert(Record<T> data, int funcID)
 {
 	int hashVal = hashFunc(data.key, funcID);
 	int location = hashVal * sizeBuck;
-	if(hashArray[location].occupied == false) // insert into the first slot of bucket
-	{
-		hashArray[location].data = data;
-		hashArray[location].occupied = true;
-		cout << "Successful\n";
-		return true;
+	// Try to insert into the bucket
+	for (int i = 0; i < sizeBuck; i++) {
+		int idx = location + i;
+		if (!hashArray[idx].occupied) {
+			hashArray[idx].data = data;
+			hashArray[idx].occupied = true;
+			cout << "Successful\n";
+			return true;
+		}
 	}
+	// If bucket is full, insert into overflow bucket
+	for (int i = 0; i < OVERFLOW_SIZE; i++) {
+		if (!overBuck[i].occupied) {
+			overBuck[i].data = data;
+			overBuck[i].occupied = true;
+			cout << "Inserted in overflow bucket\n";
+			return true;
+		}
+	}
+	cout << "Failed: Overflow bucket full\n";
 	return false;
 }
 //---------------------------------------------------------
@@ -83,12 +115,28 @@ bool HashTable<T>::search(int key, T& value, int funcID)
 {
 	int hashVal = hashFunc(key, funcID);
 	int location = hashVal * sizeBuck;
-	if(hashArray[location].occupied == true && hashArray[location].data.key == key) // search for the first slot of bucket
-	{
-		value = hashArray[index].data.value;
-		cout << "Exist\n";
-		return true;
-	}		
+	// Search in the bucket
+	for (int i = 0; i < sizeBuck; i++) {
+		int idx = location + i;
+		if (hashArray[idx].occupied && hashArray[idx].data.key == key) {
+			value = hashArray[idx].data.value;
+			cout << "Exist\n";
+			return true;
+		}
+		// If slot is empty, key cannot be in this bucket
+		if (!hashArray[idx].occupied) {
+			cout << "NOT exist\n";
+			return false;
+		}
+	}
+	// If bucket is full, search in overflow bucket
+	for (int i = 0; i < OVERFLOW_SIZE; i++) {
+		if (overBuck[i].occupied && overBuck[i].data.key == key) {
+			value = overBuck[i].data.value;
+			cout << "Exist (in overflow)\n";
+			return true;
+		}
+	}
 	cout << "NOT exist\n";
 	return false;
 }
