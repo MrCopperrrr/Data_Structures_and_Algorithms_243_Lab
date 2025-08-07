0983891fb27e819cb27a14d00588265401d51146
MrCopperrrr <ntphat0802@gmail.com>
2025-08-07 10:22:22 +0700

    Upload SortAlgorithm.h

---
+++ Lab 5/Lab 5_1/SortAlgorithms.h
@@ -0,0 +1,15 @@
+#ifndef SORT_ALGORITHMS_H
+#define SORT_ALGORITHMS_H
+
+#include <vector>
+
+void bubbleSort(std::vector<int>& arr);
+void selectionSort(std::vector<int>& arr);
+void insertionSort(std::vector<int>& arr);
+void heapSort(std::vector<int>& arr);
+void mergeSort(std::vector<int>& arr);
+void quickSort(std::vector<int>& arr);
+
+void shellSort(std::vector<int>& arr);
+
+#endif // SORT_ALGORITHMS_H
